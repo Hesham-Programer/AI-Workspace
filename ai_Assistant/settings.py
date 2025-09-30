674086eb18e14c18e9e5455ed904a6929d4bac7e
Hesham <nasreldinheshamword@gmail.com>
2025-09-30 20:25:12 +0100

    switch database to MySQL

--- ai_Assistant/settings.py
+++ ai_Assistant/settings.py
@@ -80,10 +80,20 @@ WSGI_APPLICATION = 'ai_Assistant.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_db',
+        'USER': 'django_user',
+        'PASSWORD': 'strongpassword',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+# DATABASES = {d
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
